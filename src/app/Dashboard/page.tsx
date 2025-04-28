0247731ef89945454227e04bc0510fff8097b7ea
Clerk <108710327+Annouymous@users.noreply.github.com>
2025-04-28 07:26:32 -0700

    first commit_2

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,23 @@
+import { columns, Payment } from "@/data/columns";
+import { DataTable } from "@/data/data-table";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../config";
+
+async function fetchUsers() {
+  const querySnapshot = await getDocs(collection(db, "Collage_users"));
+  const usersData = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return usersData;
+}
+
+export default async function DemoPage() {
+  const data = await fetchUsers();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
---
+++ src/data/columns.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowDown } from "lucide-react";
+export type Payment = {
+  username: string;
+  status: string;
+  score: number;
+  mobile: string;
+  birthYear: string;
+  progress: {
+    currentQuestion: number;
+    score: number;
+  };
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "username",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Full Name
+        <ArrowDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+  },
+  {
+    accessorKey: "mobile",
+    header: "Mobile Number",
+  },
+  {
+    accessorKey: "birthYear",
+    header: "Birth Year",
+  },
+  {
+    accessorKey: "score",
+
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Score
+        <ArrowDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      const status = row.getValue("score") as string;
+      return (
+        <div className="w-full font-extrabold text-lg  justify-center items-center ">
+          {status}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: () => <div>Status</div>,
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+
+      return (
+        <div className="w-full font-medium justify-center items-center ">
+          {status === "fail" && (
+            <div className="bg-red-600/25 text-center rounded-md p-2">
+              <span className="text-red-600">Failed</span>
+            </div>
+          )}
+          {status === "passed" && (
+            <div className="bg-green-600/25 text-center rounded-md p-2">
+              <span className="text-green-600 ">Passed</span>
+            </div>
+          )}
+        </div>
+      );
+    },
+  },
+];
