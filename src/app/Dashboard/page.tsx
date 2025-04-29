255ce8c2d5550dfee59439d32171dc8ccf1ed0cd
Clerk <108710327+Annouymous@users.noreply.github.com>
2025-04-28 23:22:19 -0700

    Update page.tsx

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,22 +1,26 @@
+"use client";
 import { columns, Payment } from "@/data/columns";
 import { DataTable } from "@/data/data-table";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../config";
+import { useEffect, useState } from "react";
 
-async function fetchUsers() {
-  const querySnapshot = await getDocs(collection(db, "Collage_users"));
-  const usersData = querySnapshot.docs.map((doc) => ({
-    ...doc.data(),
-  }));
-  return usersData;
-}
-
-export default async function DemoPage() {
-  const data = await fetchUsers();
+export default function DemoPage() {
+  const [users, setUsers] = useState<Payment[]>([]);
+  async function fetchUsers() {
+    const querySnapshot = await getDocs(collection(db, "Collage_users"));
+    const usersData = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+    }));
+    setUsers(usersData);
+  }
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={users} />
     </div>
   );
 }
